f75471b07eb19ecd991582e4eb3f6ed12a272356
junichi11 <junichi0206@gmail.com>
2017-09-27 13:06:53 +0900

    Remove an unused import

--- src/main/java/com/junichi11/netbeans/modules/textlint/command/Textlint.java
+++ src/main/java/com/junichi11/netbeans/modules/textlint/command/Textlint.java
@@ -23,7 +23,6 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.StringReader;
